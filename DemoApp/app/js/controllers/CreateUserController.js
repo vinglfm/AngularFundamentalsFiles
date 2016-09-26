80dc4947b0fc8d843b7de7245b64aa9ddc13aae0
Andrew Pryshchepa <vinglfm@gmail.com>
2016-09-26 19:14:22 +0300

    Solution for loading gravatar on email update

--- DemoApp/app/js/controllers/CreateUserController.js
+++ DemoApp/app/js/controllers/CreateUserController.js
@@ -2,7 +2,8 @@
 
 app.controller("CreateUserController", ["$scope", function($scope) {
   $scope.loadGravatar = function(email) {
-    var userEmail = email | "";
+    console.log(email);
+    var userEmail = email || "";
     return 'https://www.gravatar.com/avatar/'+ calcMD5(userEmail) +'?s=200';
   };
 }]);
